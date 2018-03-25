0f7e8e6dc1c4cbcd4ace4064faf612d62e65593d
sumAlbert <982675088@qq.com>
2018-03-26 00:05:23 +0800

    some test

--- document.js
+++ document.js
@@ -73,3 +73,55 @@
         print(currentLineNum+currentLineReverseNum);
     }
 })();
+//网易（DFS）60%
+(function () {
+    var line;
+    while(line=readline()){
+        var lineArr = line.split(" ");
+        var routeInfoLine = readline();
+        var routeInfoLineArr = routeInfoLine.split(" ");
+        var n = lineArr[0];
+        var l = lineArr[1];
+        var routes = [];
+        var saveStack = [];
+        var resultAtLast = 0;
+        var resultMax = 1;
+        for(var i = 0;i<n;i++){
+            saveStack[i] = 0;
+        }
+        for(var i=1;i<n;i++){
+            var targetCity = parseInt(routeInfoLineArr[i-1]);
+            if(routes[i]==null){
+                routes[i]=[];
+            }
+            if(routes[targetCity]==null){
+                routes[targetCity]=[];
+            }
+            routes[i].push(targetCity);
+            routes[targetCity].push(i);
+        }
+        DFS(0,parseInt(l)+1);
+        print(resultMax);
+    }
+
+    function DFS(currentCity,left){
+        if(left===0){
+            resultAtLast = 0;
+            saveStack.forEach(function(v){
+                if(v!=0){
+                    resultAtLast++;
+                }
+            });
+            if(resultMax<resultAtLast){
+                resultMax=resultAtLast;
+            }
+            return null;
+        }
+        saveStack[currentCity] = saveStack[currentCity] + 1;
+        var currentRouteInfo = routes[currentCity];
+        for(var i=0;i<currentRouteInfo.length;i++){
+            DFS(currentRouteInfo[i],left-1);
+        }
+        saveStack[currentCity] = saveStack[currentCity] - 1;
+    }
+})();
